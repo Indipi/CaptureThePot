0fb7c3de03dc78417e0677c16845ec106059c3ba
Indipi <53028712+Indipi@users.noreply.github.com>
2019-07-25 12:31:28 +1200

    Version 12
    Working final version

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace CaptureThePot
 {
 
-    public class Position
+    public class Positions
     {
         public double A_Position { get; set; }
         public double B_Position { get; set; }
@@ -49,15 +49,15 @@ namespace CaptureThePot
             double C_position; // position of C, number between 0 - RESOLUTION
             double D_position; // position of D, number between 0 - RESOLUTION
 
-            double bestAArea = 0;
-            List<Position> Best_A_Position_List = new List<Position>();
-            int NumberOfBestAPositions = 1;
+            double best_A_Area = 0;
+            List<Positions> Best_A_Positions = new List<Positions>();
+            int Num_Best_A_Positions = 1;
 
             for (A_position = 0; A_position <= (RESOLUTION / 2); A_position++)
             {
-                double bestBArea = 0;
-                List<Position> Best_B_Position_List = new List<Position>();
-                int NumberOfBestBPositions = 1;
+                double best_B_Area = 0;
+                List<Positions> Best_B_Positions = new List<Positions>();
+                int Num_Best_B_Positions = 1;
 
                 Console.Clear();
                 Console.WriteLine("Thinking...");
@@ -70,9 +70,9 @@ namespace CaptureThePot
                         continue;
                     }
 
-                    double bestCArea = 0;
-                    List<Position> Best_C_Position_List = new List<Position>();
-                    int NumberOfBestCPositions = 1;
+                    double best_C_Area = 0;
+                    List<Positions> Best_C_Positions = new List<Positions>();
+                    int Num_Best_C_Positions = 1;
 
                     for (C_position = 0; C_position <= RESOLUTION; C_position++)
                     {
@@ -81,10 +81,10 @@ namespace CaptureThePot
                             continue;
                         }
 
-                        double bestDArea = 0;
-                        List<Position> Best_D_Position_List = new List<Position>();
+                        double best_D_Area = 0;
+                        List<Positions> Best_D_Positions = new List<Positions>();
 
-                        int NumberOfBestDPositions = 1;
+                        int Num_Best_D_Positions = 1;
 
                         for (D_position = 0; D_position <= RESOLUTION; D_position++)
                         {
@@ -93,143 +93,144 @@ namespace CaptureThePot
                                 continue;
                             }
 
-                            double areaD = FindArea(4, A_position, B_position, C_position, D_position, RESOLUTION);
+                            double area_D = FindArea(4, A_position, B_position, C_position, D_position, RESOLUTION);
 
-                            Position position = new Position{ A_Position = A_position, B_Position = B_position, C_Position = C_position, D_Position = D_position,
+                            Positions position = new Positions{ A_Position = A_position, B_Position = B_position, C_Position = C_position, D_Position = D_position,
                                 A_Area = FindArea(1, A_position, B_position, C_position, D_position, RESOLUTION),
                                 B_Area = FindArea(2, A_position, B_position, C_position, D_position, RESOLUTION),
                                 C_Area = FindArea(3, A_position, B_position, C_position, D_position, RESOLUTION),
-                                D_Area = areaD };
+                                D_Area = area_D
+                            };
 
-                            if (areaD > bestDArea)
+                            if (area_D > best_D_Area)
                             {
-                                NumberOfBestDPositions = 1;
+                                Num_Best_D_Positions = 1;
 
                                 // Clear D Position List
                                 // Then add the current position to the list
-                                Best_D_Position_List.Clear();
-                                Best_D_Position_List.Add(position);
-                                bestDArea = areaD;
+                                Best_D_Positions.Clear();
+                                Best_D_Positions.Add(position);
+                                best_D_Area = area_D;
                             }
-                            else if (areaD == bestDArea)
+                            else if (area_D == best_D_Area)
                             {
                                 // Multiple Best Positions
                                 // Add current position to the D Position List
-                                Best_D_Position_List.Add(position);
-                                NumberOfBestDPositions = NumberOfBestDPositions + 1;
+                                Best_D_Positions.Add(position);
+                                Num_Best_D_Positions = Num_Best_D_Positions + 1;
                             }
                         }
 
-                        double areaC = 0;
-                        foreach(Position p in Best_D_Position_List)
+                        double area_C = 0;
+                        foreach(Positions p in Best_D_Positions)
                         {
-                            areaC = areaC + p.C_Area;
+                            area_C = area_C + p.C_Area;
                         }
 
-                        areaC = areaC / NumberOfBestDPositions;
+                        area_C = area_C / Num_Best_D_Positions;
 
-                        if (areaC > bestCArea)
+                        if (area_C > best_C_Area)
                         {
                             // Clear the C Position List
                             // Then set entire Cposition List equal to D position List
-                            Best_C_Position_List.Clear();
-                            Best_C_Position_List.AddRange(Best_D_Position_List);
-                            NumberOfBestCPositions = NumberOfBestDPositions;
-                            bestCArea = areaC;
+                            Best_C_Positions.Clear();
+                            Best_C_Positions.AddRange(Best_D_Positions);
+                            Num_Best_C_Positions = Num_Best_D_Positions;
+                            best_C_Area = area_C;
                         }
-                        else if (areaC == bestCArea)
+                        else if (area_C == best_C_Area)
                         {
                             // Multiple Best Positions
                             // Add entire D position list to end of C position List
-                            Best_C_Position_List.AddRange(Best_D_Position_List);
-                            NumberOfBestCPositions = NumberOfBestCPositions + NumberOfBestDPositions;
+                            Best_C_Positions.AddRange(Best_D_Positions);
+                            Num_Best_C_Positions = Num_Best_C_Positions + Num_Best_D_Positions;
                         }
                     }
 
-                    double areaB = 0;
-                    foreach (Position p in Best_C_Position_List)
+                    double area_B = 0;
+                    foreach (Positions p in Best_C_Positions)
                     {
-                        areaB = areaB + p.B_Area;
+                        area_B = area_B + p.B_Area;
                     }
 
-                    areaB = areaB / NumberOfBestCPositions;
+                    area_B = area_B / Num_Best_C_Positions;
 
-                    if (areaB > bestBArea)
+                    if (area_B > best_B_Area)
                     {
                         // Clear the B Position List
                         // Then set entire B position List equal to C position List
-                        Best_B_Position_List.Clear();
-                        Best_B_Position_List.AddRange(Best_C_Position_List);
-                        NumberOfBestBPositions = NumberOfBestCPositions;
-                        bestBArea = areaB;
+                        Best_B_Positions.Clear();
+                        Best_B_Positions.AddRange(Best_C_Positions);
+                        Num_Best_B_Positions = Num_Best_C_Positions;
+                        best_B_Area = area_B;
 
                     }
-                    else if (areaB == bestBArea)
+                    else if (area_B == best_B_Area)
                     {
                         // Multiple Best Positions
                         // Add entire C position list to end of B position List
-                        Best_B_Position_List.AddRange(Best_C_Position_List);
-                        NumberOfBestBPositions = NumberOfBestBPositions + NumberOfBestCPositions;
+                        Best_B_Positions.AddRange(Best_C_Positions);
+                        Num_Best_B_Positions = Num_Best_B_Positions + Num_Best_C_Positions;
                     }
                 }
 
-                double areaA = 0;
-                foreach (Position p in Best_B_Position_List)
+                double area_A = 0;
+                foreach (Positions p in Best_B_Positions)
                 {
-                    areaA = areaA + p.A_Area;
+                    area_A = area_A + p.A_Area;
                 }
 
-                areaA = areaA / NumberOfBestBPositions;
+                area_A = area_A / Num_Best_B_Positions;
 
-                if (areaA > bestAArea)
+                if (area_A > best_A_Area)
                 {
                     // Clear the A Position List
                     // Then set entire A position List equal to B position List
-                    Best_A_Position_List.Clear();
-                    Best_A_Position_List.AddRange(Best_B_Position_List);
-                    NumberOfBestAPositions = NumberOfBestBPositions;
-                    bestAArea = areaA;
+                    Best_A_Positions.Clear();
+                    Best_A_Positions.AddRange(Best_B_Positions);
+                    Num_Best_A_Positions = Num_Best_B_Positions;
+                    best_A_Area = area_A;
 
                 }
-                else if (areaA == bestAArea)
+                else if (area_A == best_A_Area)
                 {
                     // Multiple Best Positions
                     // Add entire B position list to end of A position List
-                    Best_A_Position_List.AddRange(Best_B_Position_List);
-                    NumberOfBestAPositions = NumberOfBestAPositions + NumberOfBestBPositions;
+                    Best_A_Positions.AddRange(Best_B_Positions);
+                    Num_Best_A_Positions = Num_Best_A_Positions + Num_Best_B_Positions;
                 }
 
             }
 
-            double uniqueAPos = 0;
-            int countPosA = 0;
-            foreach (Position p in Best_A_Position_List)
+            double unique_Pos_A = 0;
+            int count_Pos_A = 0;
+            foreach (Positions p in Best_A_Positions)
             {
-                if (uniqueAPos != p.A_Position)
+                if (unique_Pos_A != p.A_Position)
                 {
-                    countPosA++;
-                    uniqueAPos = p.A_Position;
+                    count_Pos_A++;
+                    unique_Pos_A = p.A_Position;
                 }
             }
 
             Console.Clear();
             Console.WriteLine("Number of possible positions (from 0 to 0.5 inclusive): \n");
-            Console.WriteLine(countPosA);
+            Console.WriteLine(count_Pos_A);
             Console.WriteLine("\n\nBest position(s) are: \n");
 
-            uniqueAPos = 0;
-            foreach (Position p in Best_A_Position_List)
+            unique_Pos_A = 0;
+            foreach (Positions p in Best_A_Positions)
             {
-                if (uniqueAPos != p.A_Position)
+                if (unique_Pos_A != p.A_Position)
                 {
                     // Convert back to a value between 0 and 1 based on RESOLUTION and print
                     Console.WriteLine((p.A_Position) / RESOLUTION);
-                    uniqueAPos = p.A_Position;
+                    unique_Pos_A = p.A_Position;
                 }
             }
 
             Console.WriteLine("\n\nWith an expected chance of winning of: \n");
-            Console.WriteLine(((bestAArea) / RESOLUTION) * 100 + " %");
+            Console.WriteLine(((best_A_Area) / RESOLUTION) * 100 + " %");
 
             Console.WriteLine("\n\nNote: Mirrored positions also work equally well. ");
             Console.WriteLine("i.e. 1 - minus the above position is also a valid position.");
@@ -237,7 +238,7 @@ namespace CaptureThePot
 
             Console.WriteLine("\n\n");
 
-            // Below function prints entire list of positions for A
+            // Below function prints entire list of best positions for A including other player positions
             // Commented out as is used for debugging purposes
             //PrintAllPositions(Best_A_Position_List, RESOLUTION);
 
@@ -283,10 +284,10 @@ namespace CaptureThePot
 
         // The following function will print all positions in a list
         // Used for debugging purposes
-        static void PrintAllPositions(List<Position> list_of_positions, int RESOLUTION)
+        static void PrintAllPositions(List<Positions> list_of_positions, int RESOLUTION)
         {
             Console.Write("Printing all positions... ");
-            foreach (Position p in list_of_positions)
+            foreach (Positions p in list_of_positions)
             {
                 Console.Write("\nPosition A: ");
                 Console.Write(p.A_Position / RESOLUTION);
